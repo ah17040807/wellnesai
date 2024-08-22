1633d23bb3e6c82379084b5201109f10a628208e
creator <ammarhrgic@gmail.com>
2024-08-22 23:10:13 +0200

    Updates

---
+++ precache-manifest.de472ecb8112af0a7656aaefae4e8c1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "205de466a181892c1e4ed75c78a31ce2",
+    "url": "/wellnesai/index.html"
+  },
+  {
+    "revision": "07c7017d830b115f1939",
+    "url": "/wellnesai/static/css/main.a1d975a9.chunk.css"
+  },
+  {
+    "revision": "01e4fcf3fd60fcc52357",
+    "url": "/wellnesai/static/js/2.33aeceef.chunk.js"
+  },
+  {
+    "revision": "d9e5dc2fb91f6ec79e50",
+    "url": "/wellnesai/static/js/3.9c941d3c.chunk.js"
+  },
+  {
+    "revision": "07c7017d830b115f1939",
+    "url": "/wellnesai/static/js/main.b6e719c5.chunk.js"
+  },
+  {
+    "revision": "92113b6fcaedd8954585",
+    "url": "/wellnesai/static/js/runtime~main.ba1e4aa7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6e4e765337a2e5a2fc966bd19a75de02.js"
+  "/wellnesai/precache-manifest.de472ecb8112af0a7656aaefae4e8c1b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wellnesai/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
